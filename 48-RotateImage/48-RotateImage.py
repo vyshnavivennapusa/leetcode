7b09be2133d6c259ae381db73d02a75afd81d06f
vyshnavivennapusa <vennapusavyshnavi74@gmail.com>
2026-01-31 10:41:18 +0530

    File updated at 1/31/2026, 10:41:17 AM

--- 48-RotateImage/48-RotateImage.py
+++ 48-RotateImage/48-RotateImage.py
@@ -1,4 +1,4 @@
-# Last updated: 1/31/2026, 10:21:01 AM
+# Last updated: 1/31/2026, 10:41:17 AM
 1class Solution(object):
 2    def rotate(self, matrix):
 3        """
@@ -6,12 +6,11 @@
 5        :rtype: None Do not return anything, modify matrix in-place instead.
 6        """
 7        n=len(matrix)
-8        ans=[[0]*n for x in range(0,n)]
-9        for i in range(0,n):
-10            for j in range(0,n):
-11                ans[j][n-1-i]=matrix[i][j]
+8        for i in range(0,n):
+9            for j in range(i+1,n):
+10                if i!=j:
+11                    matrix[j][i],matrix[i][j]=matrix[i][j],matrix[j][i]
 12        for i in range(0,n):
-13            for j in range(0,n):
-14                matrix[i][j]=ans[i][j]
+13            matrix[i].reverse()
+14        
 15        
-16        
