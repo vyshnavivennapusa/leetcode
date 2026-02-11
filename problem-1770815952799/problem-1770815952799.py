702270bde60df858ca8641881a1237a5bbf07f81
vyshnavivennapusa <vennapusavyshnavi74@gmail.com>
2026-02-11 18:49:13 +0530

    Create

---
+++ problem-1770815952799/problem-1770815952799.py
@@ -0,0 +1,31 @@
+# Last updated: 2/11/2026, 6:49:12 PM
+1class Solution(object):
+2    def findPeakElement(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        n=len(nums)
+8     
+9    
+10        low=1
+11        high=n-2
+12        if len(nums)==1:
+13                return 0
+14        
+15        if nums[0]>nums[1]:
+16            return 0
+17        if nums[n-1]>nums[n-2]:
+18            return n-1
+19        while(low<=high):
+20            mid=(low+high)//2
+21           
+22            if nums[mid-1]<nums[mid] and nums[mid]>nums[mid+1]:
+23                return mid
+24                
+25            elif nums[mid-1]<nums[mid]:
+26                low=mid+1
+27            else:
+28                high=mid-1
+29        return -1
+30
